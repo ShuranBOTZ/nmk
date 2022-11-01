728688c6887c1ac41505b4479a6481620f8d65db
ShuranBOTZ <114658595+ShuranBOTZ@users.noreply.github.com>
2022-11-01 22:13:03 +0700

    Update main-menulist.js

--- plugins/main-menulist.js
+++ plugins/main-menulist.js
@@ -9,9 +9,9 @@ import fetch from 'node-fetch'
 
 const defaultMenu = {
 before: ` `.trimStart(),
-header: '⃟⃟☰⃟⃟ᭁ═━┈━┈༓ *%category* ',
+header: '⋘══════∗ {•『 *%category* 』•} ∗══════ ⋙',
 body: `┆➨ %cmd %isPremium %islimit`,
-footer: `⃟⃟⃟⃟࿑⃟⃟⃟࿐═┈༓᭄༤\n`,
+footer: `⋘══════∗ {•『 父 』•} ∗══════ ⋙\n`,
 after: ` `,
 }
 let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
