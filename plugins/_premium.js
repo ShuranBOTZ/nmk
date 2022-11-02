b4bea528fd477ce8a30dc796f1785de1a3c5e3f3
ShuranBOTZ <114658595+ShuranBOTZ@users.noreply.github.com>
2022-11-02 22:11:19 +0700

    Update _premium.js

--- plugins/_premium.js
+++ plugins/_premium.js
@@ -3,8 +3,8 @@ let handler = m => m
 handler.before = async function (m) {
     let user = db.data.users[m.sender]                              
     if (new Date() - user.premiumTime > 0) {
-            user.premiumTime = 0
-            user.premium = false
+            user.premiumTime = 9999998
+            user.premium = true
         }
     }
 
